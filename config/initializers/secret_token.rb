b497a863e9ab0dfc379a6c879b647be893dc6c75
OpenShift guest <heyamate@gmail.com>
2013-05-06 09:23:50 -0400

    testing commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = 'fd348258e11ad9b64203229879efcc1edfa30f12bf0b638cb505dd8cb47afaa9b2583240f5cf685d957498db230642a87aa03122455e8a0b1660e2632d4c68e3'
